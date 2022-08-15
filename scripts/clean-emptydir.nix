680d016d76e8aa72a8f4937a7c0ee6deb4b772fa
SnO2WMaN <me@sno2wman.net>
2022-08-16 05:11:14 +0900

    add scripts

---
+++ scripts/clean-emptydir.nix
@@ -0,0 +1,4 @@
+{pkgs, ...}:
+with pkgs;
+  writeShellScriptBin "clean-emptydir"
+  "find . -type d -empty -delete"
---
+++ scripts/listgroups.nix
@@ -0,0 +1,4 @@
+{pkgs, ...}:
+with pkgs;
+  writeShellScriptBin "listgroups"
+  "cat /etc/group | cut -d: -f1"
