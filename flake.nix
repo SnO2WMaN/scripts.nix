1c86fa2cc033b23f37cee9eca539ba4c7119184f
SnO2WMaN <me@sno2wman.net>
2023-02-11 04:18:35 +0900

    refactoring everything

--- flake.nix
+++ flake.nix
@@ -1,25 +1,20 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    # dev
     flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
-    devshell.url = "github:numtide/devshell";
-    treefmt-upstream.url = "github:numtide/treefmt";
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-    devshell,
-    treefmt-upstream,
     ...
-  }:
+  } @ inputs:
     {
       overlays.default = import ./overlay.nix;
     }
@@ -27,20 +22,30 @@
       system: let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [
+          overlays = with inputs; [
             devshell.overlay
             self.overlays.default
-            (_: _: {treefmt = treefmt-upstream.packages.${system}.treefmt;})
           ];
         };
-        inherit (pkgs) lib;
+        inherit (flake-utils.lib) mkApp flattenTree;
       in {
         devShells.default = pkgs.devshell.mkShell {
-          imports = [(pkgs.devshell.importTOML ./devshell.toml)];
+          packages = with pkgs; [
+            act
+            alejandra
+            taplo-cli
+            treefmt
+          ];
         };
-        packages = import ./scripts.nix (name: (pkgs.${name}));
-        apps = import ./scripts.nix (name: (flake-utils.lib.mkApp {drv = self.packages.${system}.${name};}));
-        checks = import ./scripts.nix (name: (self.packages.${system}.${name}));
+        packages = flattenTree {
+          inherit (pkgs) clean-emptydir listgroups listpath;
+        };
+        apps = with (self.packages.${system}); {
+          clean-emptydir = mkApp {drv = clean-emptydir;};
+          listgroups = mkApp {drv = listgroups;};
+          listpath = mkApp {drv = listpath;};
+        };
+        checks = self.packages.${system};
       }
     );
 }
--- overlay.nix
+++ overlay.nix
@@ -1,3 +1,5 @@
-final: prev: (
-  import ./scripts.nix (name: final.callPackage (import ./scripts/${name}.nix) {})
-)
+final: prev: {
+  clean-emptydir = final.callPackage ./scripts/clean-emptydir.nix {};
+  listgroups = final.callPackage ./scripts/listgroups.nix {};
+  listpath = final.callPackage ./scripts/listpath.nix {};
+}
